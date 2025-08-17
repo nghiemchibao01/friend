fcff207b46366ff52effc8592a5d6f8b98972864
nghiemchibao01 <nghiemchibao01@gmail.com>
2025-08-17 19:09:14 +0700

    Create new Repo

---
+++ src/main/java/com/friend/friend/FriendApplication.java
@@ -0,0 +1,18 @@
+package com.friend.friend;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendApplication {
+
+	public static void main(String[] args) {
+		var env = Dotenv.load();
+		var mongoDbUri = env.get("MONGODB_URI", "");
+
+		System.setProperty("MONGODB_URI", mongoDbUri);
+		SpringApplication.run(FriendApplication.class, args);
+	}
+
+}
