72541f187f2cbadb5dbef2b27a6ea3cb95ee838a
nghiemchibao01 <nghiemchibao01@gmail.com>
2025-08-19 16:16:39 +0700

    Update Router

--- src/main/java/com/friend/friend/router/friend/FriendRouter.java
+++ src/main/java/com/friend/friend/router/friend/FriendRouter.java
@@ -5,19 +5,19 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.*;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class FriendRouter {
 	@Bean
 	public RouterFunction<ServerResponse> friendRoutes(@NotNull FriendHandler handler) {
-		return route(GET("/friends"), handler::getAllFriends)
-				.andRoute(GET("/friends/{id}"), handler::getFriendById)
-				.andRoute(POST("/friends"), handler::createFriend)
-				.andRoute(PUT("/friends/{id}"), handler::updateFriend)
-				.andRoute(DELETE("/friends/{id}"), handler::deleteFriend);
+		return RouterFunctions.route()
+				.POST("/friends", handler::createFriend)
+				.GET("/friends", handler::getAllFriends)
+				.GET("/friends/{id}", handler::getFriendById)
+				.PUT("/friends/{id}", handler::updateFriend)
+				.DELETE("/friends/{id}", handler::deleteFriend)
+				.build();
 	}
 }
