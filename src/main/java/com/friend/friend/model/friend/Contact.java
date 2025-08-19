a19faacbc51c7dcdde75f2408def26db689753e8
nghiemchibao01 <nghiemchibao01@gmail.com>
2025-08-19 19:51:55 +0700

    Update tabs

--- src/main/java/com/friend/friend/model/friend/Contact.java
+++ src/main/java/com/friend/friend/model/friend/Contact.java
@@ -4,37 +4,37 @@
 package com.friend.friend.model.friend;
 
 public class Contact {
-    private String FbUrl;
-    private  String InsUrl;
-    private String phoneNum;
-    private String email;
+	private String FbUrl;
+	private  String InsUrl;
+	private String phoneNum;
+	private String email;
 
-    // Getters & Setters
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	// Getters & Setters
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
 
-    public String getInsUrl() {
-        return InsUrl;
-    }
-    public void setInsUrl(String insUrl) {
-        InsUrl = insUrl;
-    }
+	public String getInsUrl() {
+		return InsUrl;
+	}
+	public void setInsUrl(String insUrl) {
+		InsUrl = insUrl;
+	}
 
-    public String getFbUrl() {
-        return FbUrl;
-    }
-    public void setFbUrl(String fbUrl) {
-        FbUrl = fbUrl;
-    }
+	public String getFbUrl() {
+		return FbUrl;
+	}
+	public void setFbUrl(String fbUrl) {
+		FbUrl = fbUrl;
+	}
 }
--- src/main/java/com/friend/friend/model/friend/Friend.java
+++ src/main/java/com/friend/friend/model/friend/Friend.java
@@ -8,47 +8,46 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "friends")
 public class Friend {
-    @Id
-    private String id;
-    private String name;
+	@Id
+	private String id;
+	private String name;
+	private Contact contact;
+	private Parent parent;
+	private Info info;
 
-    private Contact contact;
-    private Parent parent;
-    private Info info;
+	// Getters & Setters
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    // Getters & Setters
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+	public Contact getContact() {
+		return contact;
+	}
+	public void setContact(Contact contact) {
+		this.contact = contact;
+	}
 
-    public Contact getContact() {
-        return contact;
-    }
-    public void setContact(Contact contact) {
-        this.contact = contact;
-    }
+	public Parent getParent() {
+		return parent;
+	}
+	public void setParent(Parent parent) {
+		this.parent = parent;
+	}
 
-    public Parent getParent() {
-        return parent;
-    }
-    public void setParent(Parent parent) {
-        this.parent = parent;
-    }
-
-    public Info getInfo() {
-        return info;
-    }
-    public void setInfo(Info info) {
-        this.info = info;
-    }
+	public Info getInfo() {
+		return info;
+	}
+	public void setInfo(Info info) {
+		this.info = info;
+	}
 }
